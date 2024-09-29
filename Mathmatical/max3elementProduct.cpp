7b8ac7147933dda7bcfb59402f2e1048dc81e952
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-09-29 22:52:20 +0530

    29 sep

---
+++ Mathmatical/max3elementProduct.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+#include<vector>
+
+using namespace std; 
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int posMax = nums[n-1]*nums[n-2]*nums[n-3];
+        int negMax = nums[0]*nums[1]*nums[n-1];
+        return max(posMax,negMax);
+        
+    }
+};
