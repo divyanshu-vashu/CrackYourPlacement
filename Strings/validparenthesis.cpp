ca926c95eb5c9cc81374be8455b851da191f03f9
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-10-01 22:16:19 +0530

    1 oct

---
+++ Strings/validparenthesis.cpp
@@ -0,0 +1,28 @@
+
+#include<iostream>
+#include<vector>
+#include<map>
+#include<queue>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st; 
+        for (char c : s) { 
+            if (c == '(' || c == '{' || c == '[') { 
+                st.push(c); 
+            } else { 
+                if (st.empty() ||
+                    (c == ')' && st.top() != '(') || 
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; 
+                }
+                st.pop(); 
+            }
+        }
+        return st.empty();
+    }
+};
