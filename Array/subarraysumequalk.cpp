c5cd1fc120850c2b1bbc1534602b7ccd7fec58de
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-10-05 10:42:53 +0530

    5 oct

--- Array/subarraysumequalk.cpp
+++ Array/subarraysumequalk.cpp
@@ -1,37 +1,40 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        int n= nums.size();
+        m[0]=1;
+        int presum=0,cnt=0;
+        for(int i=0;i<n;i++){
+            presum+=nums[i];
 
-#include<iostream>
-#include<vector>
-#include<unordered_map>
-using namespace std;
+            int rem= presum-k;
+            if(m.find(rem)!=m.end()) cnt+=m[rem];
+
+            m[presum]++;
+
+        }
+        return cnt;
+    }
+};
+
+/*
 
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        int count = 0;
-        unordered_map<long long,int> hash_map;
-        long long prefixSum = 0;
-
-        for(int i=0;i<nums.size();i++) {
-            prefixSum += nums[i];
-
-            if(prefixSum == k) {
-                count++;
-            }
+        int n=nums.size();
+        int c=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
             
-            long long rem = prefixSum - k;
-            if(hash_map.find(rem) != hash_map.end()) {
-                count += hash_map[rem];
-            }
-
-            if(hash_map.find(prefixSum) != hash_map.end()) {
-                hash_map[prefixSum]++;
-            } else {
-                hash_map[prefixSum] = 1; 
+            for(int j=i;j<n;j++){
+                sum+=nums[j];
+                if(sum==k) c++;;
             }
         }
-
-        return count;
+        return c;
     }
 };
+
+*/
