19e22ea74cc882273cbd4c6b42929ace9265eacf
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-10-22 16:23:55 +0530

    22 oct

--- python/numpy/reshape.py
+++ python/numpy/reshape.py
@@ -10,4 +10,4 @@ arr2 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,9])
 
 # newarr2 = arr.reshape(2, 3, 2) #array to will have error because it can't shape into 2,3,2
 
-print(newarr2)
+# print(newarr2)
