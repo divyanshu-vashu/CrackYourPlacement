3c999e47d900d7e8fc98222d16cadf82b68ac99e
divyanshu-vashu <vashusingh2004.jan@gmail.com>
2024-10-25 13:58:53 +0530

    25 oct

---
+++ BItwise/1611_minimumonebit_to_make_zero.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        if(n==0) return 0;
+        vector<long long>f(31,0);
+        f[0]=1;
+        for(int i=1;i<=30;i++){
+            f[i]=2*f[i-1]+1;
+        }
+        int result=0;
+        int sign=1;
+        for(int i=30;i>=0;i--){
+            int ith_bit=(1<<i)&n;
+            if(ith_bit==0) continue;
+
+            if(sign>0){
+                result+=f[i];
+            }else{
+                result-=f[i];
+            }
+
+            sign=sign*(-1);
+
+
+        }
+        return result;
+    }
+};
